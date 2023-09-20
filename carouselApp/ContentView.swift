8678b59b87ce096f14040e1fead79d2165266a3c
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2023-09-20 11:25:22 +0530

    Add files via upload

---
+++ carouselApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  carouselApp
+//
+//  Created by Aditya Tyagi  on 20/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ carouselApp/carouselAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  carouselAppApp.swift
+//  carouselApp
+//
+//  Created by Aditya Tyagi  on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct carouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
